209d7e6681b2d5d39015c4befad16cfc874e0305
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2023-03-28 12:46:23 +0200

    typescript fix

---
+++ src/components/types.ts
@@ -0,0 +1,30 @@
+export enum Level {
+  Fönstertvätt = "Fönstertvätt",
+  Basic = "Basic Städning",
+  Topp = "Topp Städning",
+  Diamant = "Diamant Städning"
+}
+
+export type Booking = {
+  _id: string;
+  date: Date;
+  time: string;
+  serviceType: Level;
+  cleaner: string;
+};
+
+
+export type BookingListProps = {
+  bookings: Booking[];
+  onDeleteBooking: (id: string) => void;
+};
+
+export interface AddBookingFormProps {
+  addBooking: (booking: Booking) => Promise<void>;
+}
+
+export type BookingPageProps = {
+  bookings: Booking[];
+  addBooking: (booking: Booking) => Promise<void>;
+  deleteBooking: (id: string) => Promise<void>;
+};
