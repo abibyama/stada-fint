aaeb31dc38fa5944d81ffe9fb6ce95d160d62ac3
moali-98 <moali_98@hotmail.com>
2023-03-28 12:58:31 +0200

    Booking Page och Landing Page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <h1>Welcome to CleanBooking!</h1>
+      <p>Book your cleaning appointment today!</p>
+    </div>
+  );
+}
+
+export default LandingPage;
